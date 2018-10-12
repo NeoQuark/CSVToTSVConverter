524ae142a3021c13e7f5ae950887f5824df913d0
NeoQuark <quentin.courbin@ynov.com>
2018-10-12 08:57:34 +0200

    [Add] csvtotsv.py and aapl.csv

---
+++ csvtotsv.py
@@ -0,0 +1,32 @@
+import csv, re
+
+while True:
+	try:
+		# Looking for source csv
+		fullpath = re.findall(r"((?:[A-Z]:/|\./|/)?(?:[^.]+/)*)([^/]+)(\.csv)", input("Enter path to CSV :\n").replace('\\', '/'))[0]
+		print(fullpath)
+
+		src = ''.join(map(str,fullpath))
+		print(src)
+
+		
+		filename = fullpath[-2]
+		ext = fullpath[-1]
+		path = fullpath[0]
+
+		with open(src,'r') as csvFile, open('{}{}.tsv'.format(path, filename), 'w+') as tsvFile:
+			csvFile = csv.reader(csvFile)
+			tsvFile = csv.writer(tsvFile, delimiter='\t')
+
+			for row in csvFile:
+				tsvFile.writerow(row)
+			# print(path + filename)
+
+		print('\n-----\nOutput file => {}.tsv at {}\n'.format(filename, path))
+		break
+
+	except IndexError:
+		print("\n–––\nMake sure the full path including filename extension is correct!\n–––\n")
+		pass
+	except FileNotFoundError:
+		print("\n–––\nFile not found!\n–––\n")
